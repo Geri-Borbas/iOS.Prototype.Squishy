63b81be0ad0c9cd4bbc48c55fc6713bdd2592981
Geri Borbás <geri@eppz.eu>
2024-05-11 11:56:18 +0200

    Initial Commit

---
+++ Squishy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Squishy
+//
+//  Created by Personal on 11/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Squishy/SquishyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquishyApp.swift
+//  Squishy
+//
+//  Created by Personal on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SquishyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
